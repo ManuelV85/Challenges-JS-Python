53326d7dea884feb552dda0340bd5455d68352f1
Manuel Villate <manuel.villater@gmail.com>
2023-03-26 20:14:23 -0300

    Fizz Buzz FizzBuzz challenge in JS and python

---
+++ 01FizzBuzz/fizz_buzz_fizzbuzz.py
@@ -0,0 +1,12 @@
+def fizz_buzz():
+    for index in range(1, 101):
+        if index %3 == 0 and index % 5 == 0:
+            print ("FizzBuzz")
+        elif index %5 == 0:
+            print ("Buzz")
+        elif index %3 == 0:
+            print("Fizz")
+        else:
+            print (index)
+
+fizz_buzz()
